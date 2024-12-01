ddb25c532d3a491b24136bc11038e746b97b5bfd
Eric Hoover <ehoover3@yahoo.com>
2024-11-30 21:37:42 -0500

    init bird sprite

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -6,23 +5,25 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: { y: 200 },
+    },
   },
   scene: {
     preload: preload,
-    create: create
-  }
+    create: create,
+  },
 };
 
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+}
+
+function create() {
+  this.add.image(0, 0, "sky").setOrigin(0);
+  this.add.sprite(config.width / 2, config.height / 2, "bird").setOrigin(0);
+}
+
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
