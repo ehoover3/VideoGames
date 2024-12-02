9605c738348d34b51455e143da71b735b92d7a1a
Eric Hoover <ehoover3@yahoo.com>
2024-12-02 07:44:58 -0500

    create placeholder for flap function with onClick or onSpacebarDown functionality

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -7,7 +7,8 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      // gravity: { y: 200 },
+      gravity: { y: 200 },
+      debug: true,
     },
   },
   scene: {
@@ -30,15 +31,14 @@ let totalDelta = null;
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
   bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
-  bird.body.velocity.x = VELOCITY;
+
+  this.input.on("pointerdown", flap);
+
+  this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-function update(time, delta) {
-  if (bird.x >= config.width - bird.width) {
-    bird.body.velocity.x = -VELOCITY;
-  } else if (bird.x <= 0) {
-    bird.body.velocity.x = VELOCITY;
-  }
-}
+function update(time, delta) {}
+
+function flap() {}
 
 new Phaser.Game(config);
