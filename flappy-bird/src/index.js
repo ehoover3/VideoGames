26783346070471da5a4b5a218d6d900d3667964f
Eric Hoover <ehoover3@yahoo.com>
2024-12-02 07:32:50 -0500

    add update function

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -13,6 +13,7 @@ const config = {
   scene: {
     preload: preload,
     create: create,
+    update: update,
   },
 };
 
@@ -22,13 +23,19 @@ function preload() {
 }
 
 let bird = null;
+let totalDelta = null;
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
   bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
-  bird.body.gravity.y = 200;
-  console.log(bird.body);
-  debugger;
+  bird.body.velocity.y = 200;
+}
+
+function update(time, delta) {
+  if (totalDelta >= 1000) {
+    return;
+  }
+  totalDelta += delta;
 }
 
 new Phaser.Game(config);
