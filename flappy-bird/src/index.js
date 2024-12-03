477eed43f4da4e92da46a2eb4882105bcfae0d5d
Eric Hoover <ehoover3@yahoo.com>
2024-12-03 07:41:43 -0500

    set constants for initialBirdPosition and flapVelocity

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -26,16 +26,19 @@ function preload() {
 const VELOCITY = 200;
 
 let bird = null;
-let flapVelocity = 300;
+const flapVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 let totalDelta = null;
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
 }
+
+function restartPlayerPosition() {}
 
 function update(time, delta) {
   if (bird.y > config.height || bird.y < -bird.height) {
