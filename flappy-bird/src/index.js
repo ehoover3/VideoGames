c721edad8d6f58d8c2f9c9ec2add264030d1de62
Eric Hoover <ehoover3@yahoo.com>
2024-12-03 07:55:47 -0500

    make the pipe vertical position change each time it renders

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -24,6 +24,7 @@ let upperPipe = null;
 let lowerPipe = null;
 const pipeVerticalDistanceRange = [150, 250];
 let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
@@ -39,7 +40,7 @@ function create() {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
   bird.body.gravity.y = 400;
 
-  upperPipe = this.physics.add.sprite(400, 100, "pipe").setOrigin(0, 1);
+  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, "pipe").setOrigin(0, 1);
   lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
 
   this.input.on("pointerdown", flap);
