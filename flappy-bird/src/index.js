424c6bb863b246295a6dd4f891e0ea9f1fb1e1d7
Eric Hoover <ehoover3@yahoo.com>
2024-12-03 07:50:31 -0500

    add pipes

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -7,7 +7,6 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 400 },
       debug: true,
     },
   },
@@ -21,29 +20,40 @@ const config = {
 function preload() {
   this.load.image("sky", "assets/sky.png");
   this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
 }
 
 const VELOCITY = 200;
 
 let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
-let totalDelta = null;
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
+  bird.body.gravity.y = 400;
+
+  upperPipe = this.physics.add.sprite(400, 100, "pipe").setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, "pipe").setOrigin(0, 0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-function restartPlayerPosition() {}
-
 function update(time, delta) {
   if (bird.y > config.height || bird.y < -bird.height) {
-    alert("you lost");
+    restartBirdPosition();
   }
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
 
 function flap() {
