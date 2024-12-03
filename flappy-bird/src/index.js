896ab15ab39f2cc112c3c19cb44af5fd6d4ab6f0
Eric Hoover <ehoover3@yahoo.com>
2024-12-03 07:53:11 -0500

    make pipe vertical distance set to a variable

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -17,20 +17,22 @@ const config = {
   },
 };
 
-function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("bird", "assets/bird.png");
-  this.load.image("pipe", "assets/pipe.png");
-}
-
 const VELOCITY = 200;
 
 let bird = null;
 let upperPipe = null;
 let lowerPipe = null;
+const pipeVerticalDistanceRange = [150, 250];
+let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
 
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
+
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
+}
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
@@ -38,7 +40,7 @@ function create() {
   bird.body.gravity.y = 400;
 
   upperPipe = this.physics.add.sprite(400, 100, "pipe").setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, "pipe").setOrigin(0, 0);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
