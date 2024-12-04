ae590391fc689af9442a0dfa6afc179c3fb4e321
Eric Hoover <ehoover3@yahoo.com>
2024-12-04 07:42:35 -0500

    render multiple pipes

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -11,20 +11,21 @@ const config = {
     },
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update,
+    preload,
+    create,
+    update,
   },
 };
 
 const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 
 let bird = null;
 let upperPipe = null;
 let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
@@ -40,8 +41,17 @@ function create() {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
   bird.body.gravity.y = 400;
 
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, "pipe").setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe").setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
