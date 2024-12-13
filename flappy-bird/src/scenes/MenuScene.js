6804d6d01f5061a87107def1bb5da8003d7ad198
Eric Hoover <ehoover3@yahoo.com>
2024-12-13 08:09:25 -0500

    navigate to correct scene

--- flappy-bird/src/scenes/MenuScene.js
+++ flappy-bird/src/scenes/MenuScene.js
@@ -27,6 +27,14 @@ class MenuScene extends BaseScene {
     textGO.on("pointerout", () => {
       textGO.setStyle({ fill: "#fff" });
     });
+
+    textGO.on("pointerup", () => {
+      menuItem.scene && this.scene.start(menuItem.scene);
+
+      if (menuItem.text === "Exit") {
+        this.game.destroy(true);
+      }
+    });
   }
 }
 
