b0042360edb7561ece2644e4b2cdfb3e9b867838
Eric Hoover <ehoover3@yahoo.com>
2024-12-21 19:43:09 -0500

    make game page dynamic

---
+++ game-hub/pages/[id].js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { games } from "./games";
+import Layout from "../components/Layout";
+
+export default function GamePage() {
+  const { query } = useRouter();
+  const game = games.find((g) => g.id === query.id);
+
+  if (!game) {
+    return (
+      <Layout>
+        <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+          <h1 className='text-3xl font-bold'>Game not found</h1>
+        </div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <h2 className='text-2xl font-bold mb-4'>{game.title}</h2>
+        <h3 className='text-xl mb-4'>{game.instructions}</h3>
+        <iframe src={game.iframeSrc} width={game.width} height={game.height} style={{ border: "none" }} title={game.title}></iframe>
+      </div>
+    </Layout>
+  );
+}
--- game-hub/pages/index.js
+++ game-hub/pages/index.js
@@ -1,25 +1,19 @@
 import Link from "next/link";
+import { games } from "./games";
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h1 className='text-3xl font-bold mb-6'>🎮 Welcome to the Game Hub</h1>
+      <div>Choose a game to play</div>
       <ul className='space-y-4'>
-        <li>
-          <Link href='/flappyBird' className='text-blue-500 underline'>
-            Play Flappy Bird
-          </Link>
-        </li>
-        <li>
-          <Link href='/dinoRun' className='text-blue-500 underline'>
-            Play Dino Run
-          </Link>
-        </li>
-        <li>
-          <Link href='/platformer' className='text-blue-500 underline'>
-            Play Platformer
-          </Link>
-        </li>
+        {games.map((game) => (
+          <li key={game.id}>
+            <Link href={`/${game.id}`} className='text-blue-500 underline'>
+              {game.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
