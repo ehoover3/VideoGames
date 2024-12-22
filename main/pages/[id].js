a8c50d7001247c84691dbf651a48830cba9d72e1
Eric Hoover <ehoover3@yahoo.com>
2024-12-22 11:15:46 -0500

    remove commented out code

--- main/pages/[id].js
+++ main/pages/[id].js
@@ -1,32 +1,3 @@
-// import { useRouter } from "next/router";
-// import { games } from "../lib/games";
-// import Layout from "../components/Layout";
-
-// export default function GamePage() {
-//   const { query } = useRouter();
-//   const game = games.find((g) => g.id === query.id);
-
-//   if (!game) {
-//     return (
-//       <Layout>
-//         <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-//           <h1 className='text-3xl font-bold'>Game not found</h1>
-//         </div>
-//       </Layout>
-//     );
-//   }
-
-//   return (
-//     <Layout>
-//       <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-//         <h2 className='text-2xl font-bold mb-4'>{game.title}</h2>
-//         <h3 className='text-xl mb-4'>{game.instructions}</h3>
-//         <iframe src={game.iframeSrc} width={game.width} height={game.height} style={{ border: "none" }} title={game.title}></iframe>
-//       </div>
-//     </Layout>
-//   );
-// }
-
 import { useRouter } from "next/router";
 import { games } from "../lib/games";
 import Layout from "../components/Layout";
--- main/pages/index.js
+++ main/pages/index.js
@@ -1,24 +1,3 @@
-// import Link from "next/link";
-// import { games } from "../lib/games";
-
-// export default function Home() {
-//   return (
-//     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-//       <h1 className='text-3xl font-bold mb-6'>🎮 Welcome to the Game Hub</h1>
-//       <div>Choose a game to play</div>
-//       <ul className='space-y-4'>
-//         {games.map((game) => (
-//           <li key={game.id}>
-//             <Link href={`/${game.id}`} className='text-blue-500 underline'>
-//               {game.title}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
 import "../styles/globals.css";
 import Link from "next/link";
 import { games } from "../lib/games";
