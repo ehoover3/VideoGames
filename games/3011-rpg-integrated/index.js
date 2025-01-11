3562584c62defa5028df99ac92c34ff862257cf8
Eric Hoover <ehoover3@yahoo.com>
2025-01-11 17:27:18 -0500

    refactor to remove index.js from game folder

--- games/3011-rpg-integrated/index.js
+++ games/3011-rpg-integrated/index.js
@@ -1,8 +1,11 @@
 // index.js
 
-import { startGame } from "./game/index.js";
 import { initGameObjects } from "./game/Game.js";
 import { gameState } from "./game/Game.js";
+import { Menu } from "./game/Menu.js";
+import { STATES } from "./config/constants.js";
+import { loadOverworld } from "./game/Overworld.js";
+import { loadScanGame } from "./game/MedScanGame.js";
 
 const ASPECT_RATIO = 16 / 9;
 const { canvas, ctx } = setupCanvas("gameCanvas");
@@ -39,4 +42,24 @@ function setupCanvas(canvasId) {
   const canvas = document.getElementById(canvasId);
   const ctx = canvas.getContext("2d");
   return { canvas, ctx };
+}
+
+export function startGame({ canvas, ctx, keys, gameState, gameObjects }) {
+  let menu = new Menu(canvas, ctx, keys, gameState);
+  let overworld = { canvas, ctx, keys, gameState, gameObjects };
+  let scanGame = { canvas, ctx, keys, gameState, gameObjects };
+
+  const handleGameState = {
+    [STATES.MAIN_MENU]: () => menu.load(),
+    [STATES.OVERWORLD]: () => loadOverworld(overworld),
+    [STATES.SCAN_GAME]: () => loadScanGame(scanGame),
+  };
+
+  function gameLoop() {
+    const updateGameState = handleGameState[gameState.currentState];
+    if (updateGameState) updateGameState();
+    requestAnimationFrame(gameLoop);
+  }
+
+  gameLoop();
 }
