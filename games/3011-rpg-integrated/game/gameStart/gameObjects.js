9876d58b011f1022cc5deced3715953b3a5ea38b
Eric Hoover <ehoover3@yahoo.com>
2025-01-10 23:07:24 -0500

    refactor gameObjects

--- games/3011-rpg-integrated/game/gameStart/gameObjects.js
+++ games/3011-rpg-integrated/game/gameStart/gameObjects.js
@@ -1,59 +1,59 @@
 // game/gameObjects.js
+
 import { DIRECTION } from "../../config/constants.js";
 
-const mriImg = new Image();
-mriImg.src = "assets/images/overworld/mri.png";
+const loadedImages = {
+  mri: loadImage("assets/images/overworld/mri.png"),
+};
 
 export function initGameObjects() {
-  const player = createPlayer(100, 100, 32, 32, "blue", 4, DIRECTION.DOWN);
-  const mriMachine = {
-    x: 130,
-    y: 130,
-    width: 64,
-    height: 64,
-    image: {
-      imagePath: mriImg,
-      sourceX: 0,
-      sourceY: 0,
-      sourceWidth: 556,
-      sourceHeight: 449,
-      destiationX: 0,
-      destinationY: 0,
-      destinationWidth: 32,
-      destinationHeight: 32,
-    },
-  };
-  const xrayMachine = {
-    x: 70,
-    y: 130,
-    width: 32,
-    height: 32,
-    image: {
-      imagePath: mriImg,
-      sourceX: 0,
-      sourceY: 0,
-      sourceWidth: 556,
-      sourceHeight: 449,
-      destiationX: 0,
-      destinationY: 0,
-      destinationWidth: 32,
-      destinationHeight: 32,
-    },
-  };
+  const player = createPlayer(100, 100, 32, 32, 4, DIRECTION.DOWN);
+
+  const mriMachine = createGameObject(130, 130, 64, 64, "mri", {
+    sourceX: 0,
+    sourceY: 0,
+    sourceWidth: 556,
+    sourceHeight: 449,
+    destinationX: 0,
+    destinationY: 0,
+    destinationWidth: 32,
+    destinationHeight: 32,
+  });
+
+  const xrayMachine = createGameObject(70, 130, 32, 32, "mri", {
+    sourceX: 0,
+    sourceY: 0,
+    sourceWidth: 556,
+    sourceHeight: 449,
+    destinationX: 0,
+    destinationY: 0,
+    destinationWidth: 32,
+    destinationHeight: 32,
+  });
+
   return { player, mriMachine, xrayMachine };
 }
 
-export function createPlayer(x, y, width, height, color, speed, direction) {
-  return { x, y, width, height, color, speed, direction };
+function loadImage(src) {
+  const img = new Image();
+  img.src = src;
+  return img;
 }
 
-function createGameObject(x, y, width, height, imageKey, interactCallback) {
+export function createPlayer(x, y, width, height, speed, direction) {
+  return { x, y, width, height, speed, direction };
+}
+
+function createGameObject(x, y, width, height, imageKey, imageConfig, interactCallback) {
   return {
     x,
     y,
     width,
     height,
-    image: loadedImages[imageKey],
+    image: {
+      imagePath: loadedImages[imageKey],
+      ...imageConfig,
+    },
     interact: interactCallback || (() => {}),
   };
 }
