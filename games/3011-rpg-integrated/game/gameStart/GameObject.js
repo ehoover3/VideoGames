0ad439480fbd25c5668f21c8a14eb3ab452d2ae2
Eric Hoover <ehoover3@yahoo.com>
2025-01-11 08:27:40 -0500

    refactor GameObject class

--- games/3011-rpg-integrated/game/gameStart/GameObject.js
+++ games/3011-rpg-integrated/game/gameStart/GameObject.js
@@ -1,25 +1,30 @@
 // game/gameStart/GameObject.js
 
 export class GameObject {
-  constructor(object) {
-    this.imgPath = object.imgPath;
-    this.imgSourceY = object.imgSourceY;
-    this.imgSourceWidth = object.imgSourceWidth;
-    this.imgSourceX = object.imgSourceX;
-    this.imgSourceHeight = object.imgSourceHeight;
+  constructor({ imgPath, imgSourceX, imgSourceY, imgSourceWidth, imgSourceHeight, x, y, width = 32, height = 32 }) {
+    this.imgPath = imgPath;
+    this.imgSourceX = imgSourceX;
+    this.imgSourceY = imgSourceY;
+    this.imgSourceWidth = imgSourceWidth;
+    this.imgSourceHeight = imgSourceHeight;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-    this.x = object.x;
-    this.y = object.y;
-    this.width = object.width || 32;
-    this.height = object.height || 32;
+  getScaledDimensions(scaleX, scaleY) {
+    return {
+      scaledX: this.x * scaleX,
+      scaledY: this.y * scaleY,
+      scaledWidth: this.width * scaleX,
+      scaledHeight: this.height * scaleY,
+    };
   }
 
   draw(ctx, scaleX, scaleY) {
     const { imgSourceX, imgSourceY, imgSourceWidth, imgSourceHeight } = this;
-    const scaledX = this.x * scaleX;
-    const scaledY = this.y * scaleY;
-    const scaledWidth = this.width * scaleX;
-    const scaledHeight = this.height * scaleY;
+    const { scaledX, scaledY, scaledWidth, scaledHeight } = this.getScaledDimensions(scaleX, scaleY);
     ctx.drawImage(this.imgPath, imgSourceX, imgSourceY, imgSourceWidth, imgSourceHeight, scaledX, scaledY, scaledWidth, scaledHeight);
   }
 }
